a687ce950652547c629ac4358ee4ed6245ce963a
zbl14 <ifthereisoneday@gmail.com>
2022-08-19 09:16:07 -0700

    add properties for Busienss class

---
+++ LocalBusiness/Migrations/20220819161449_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LocalBusiness.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Business",
+                columns: table => new
+                {
+                    BusinessId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Type = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false),
+                    Phone = table.Column<string>(type: "varchar(10) CHARACTER SET utf8mb4", maxLength: 10, nullable: false),
+                    Address = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Business", x => x.BusinessId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Business");
+        }
+    }
+}
--- LocalBusiness/Model/Business.cs
+++ LocalBusiness/Model/Business.cs
@@ -1,8 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LocalBusiness.Models
 {
   public class Business
   {
-
+    public int BusinessId { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Type { get; set; }
+    [Required, StringLength(10, ErrorMessage = "Only ten digits for phone number")]
+    public string Phone { get; set; }
+    [Required]
+    public string Address { get; set; }
   }
 }
 
