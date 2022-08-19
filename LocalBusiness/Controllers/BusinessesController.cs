43e84fc74cc8586bc6dd5703d5471dc71c747c57
zbl14 <ifthereisoneday@gmail.com>
2022-08-19 09:54:02 -0700

    add CRUD funcationality

---
+++ LocalBusiness/Controllers/BusinessesController.cs
@@ -0,0 +1,119 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LocalBusiness.Models;
+using System.Linq;
+
+namespace LocalBusiness.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class BusinessesController : ControllerBase
+  {
+    private readonly LocalBusinessContext _db;
+
+    public BusinessesController(LocalBusinessContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet]
+    public async Task<List<Business>> Get(string name, string type, string phone, string address)
+    {
+      IQueryable<Business> query = _db.Business.AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+
+      if (type != null)
+      {
+        query = query.Where(entry => entry.Type == type);
+      }
+
+      if (phone != null)
+      {
+        query = query.Where(entry => entry.Phone == phone);
+      }
+
+      if (address != null)
+      {
+        query = query.Where(entry => entry.Address == address);
+      }
+
+      return await query.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Business>> GetBusiness(int id)
+    {
+      var business = await _db.Business.FindAsync(id);
+
+      if (business == null)
+      {
+        return NotFound();
+      }
+
+      return business;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Business business)
+    {
+      if (id != business.BusinessId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(business).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!BusinessExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Business>> Post(Business business)
+    {
+      _db.Business.Add(business);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetBusiness), new { id = business.BusinessId }, business);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteBusiness(int id)
+    {
+      var business = await _db.Business.FindAsync(id);
+      if (business == null)
+      {
+        return NotFound();
+      }
+
+      _db.Business.Remove(business);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    private bool BusinessExists(int id)
+    {
+      return _db.Business.Any(e => e.BusinessId == id);
+    }
+  }
+}
